90bb1e25e5374f7aea16172e5ed42458b0bdca3b
Zach Beiser <zbeiser@gmail.com>
2022-10-23 16:01:39 -0700

    added some hide and show logic to JS, reworked a couple classes and ids in HTML

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,17 @@
-/*
- 1. Design UI
-    - Draw a picture
-    - Determine where you will display feedback. 
-    - Determine what is clickable, what will recieve key input, change input
-      timers, scroll events, etc
-    - rough in the elements in HTML, style is less important
+// Declare variables: DOM hooks
 
-*/
-
-/*
-  2. Declare variables: DOM hooks
-    - In the Javascript, create variables for each of the DOM elements that will display feedback
-    - create variables for the elements that will receive input
-    - set each variable to its DOM element like:
-    
-    var theElement = document.querySelector([CSS Selector for your element]);
- */
-
-var startGameButtonEl = document.querySelector("#startGame");
+var startGameButtonEl = document.querySelector("#startGameButton");
+var startGameEl = document.querySelector(".startGame");
 var submitInitialsButtonEl = document.querySelector("#gameEnd_submit_intials");
 var restartGameButtonEl = document.querySelector("#highScores_restart");
 var clearScoresButtonEl = document.querySelector("#highScores_clearScores");
 var timerLeftEl = document.querySelector("#timeLeft");
 var timerEl = document.querySelector(".gameBoard_timer");
 var gameBoardEl = document.querySelector(".gameBoard");
+var gameEndEl = document.querySelector(".gameEnd");
+var highScoresEl = document.querySelector(".highScores");
 
-/*
- 3. Declare variables: state
-    - What are the datq that need to be kept track of? 
-    - Global state variables sometimes emerge while working on event handlers (i.e., it
-      becomes clearer what needs to be tracked across the application)
-    - state variables:
-      "State describes the status of the entire program or an individual
-       object. It could be text, a number, a boolean, or another data type.
-
-       It’s a common tool for coordinating code. For example, once you update state, a bunch of different functions can instantly react to that change."
-       https://www.freecodecamp.org/news/state-in-javascript-explained-by-cooking-a-simple-meal-2baf10a787ee/
-    - Does the state variable need to be global (i.e., used by all the event handlers) or does it only need to be local
-      to the event handler?
-*/
+// Declare variables: state
 
 var score = 0;
 var timer = null;
@@ -46,11 +19,7 @@ var timeLeft = 0;
 var currentQuestionIndex = null;
 var currentGuess = null;
 
-/*
- 4. Declare variables: constants
-    - What are the data the application needs that won't change?
-    - e.g. Math constants, pre-created content (maybe the questions and answers?)
-*/
+// Declare variables: constants
 
 var kDuration = 75;
 var kStorageKey ="timedCodingQuiz-scores";
@@ -58,33 +27,16 @@ var kQuestionList = [
 
 ];
 
-/*
- 5. Identify events
-    - Based on the variables created in Step 2, create event handlers
-
-      theElement.addeventListener([EVENT TYPE], function(event){
-        // do stuff here...
-      })
-
-    ...where [EVENT TYPE] is "click" or "change" or "keydown" or whatver
-
-    - Identify the things that should happen in the click handlers
-    - Rememember: there is always a page load event. Usually have a function for anything
-      that needs setting up at the beginning, before people interact with the 
-      page. Start the execution of this setup function at the bottom of page
-*/
+// Identify events
 
 // Event: Page load
 function init() {
   console.log("Game loading...");
 
-  var highscores = JSON.parse(localStorage.getItem(kStorageKey));
+  hideElement(gameBoardEl);
+  hideElement(gameEndEl);
+  hideElement(highScoresEl);
 
-  if(highscores) {
-
-  }
-
-  updateHighScores();
 }
 
 // Event: Click start
@@ -97,16 +49,14 @@ if (!timer) {
   timeLeft = kDuration;
   // start a timer
   timer = setInterval(handleTimerTick, 1000);
+  // hide start screen
+  hideElement(startGameEl);
+  // show game board
+  showElement(gameBoardEl);
   // choose question
   // set current guess
-  // hide start button
-  hideElement(startGameButtonEl);
   /////////////////
   // reset display
-  // hide any result message
-  hideElement();
-  // show timer 
-  showElement(timerEl);
   // show gameboard
 
 }
@@ -116,10 +66,10 @@ startGameButtonEl.addEventListener("click", handleClickStart);
 // Event: Timer tick
 
 function handleTimerTick(event) {
-  console.log("Timer ticked");
-  timer--;
+  timeLeft--;
+  console.log("Timer ticked", timeLeft);
 
-  timerEl.textContent = timeLeft;
+  timerEl.textContent = "Time left: " + timeLeft;
   if(timeLeft === 0) {
     handleGameEnd();
   }
@@ -127,15 +77,16 @@ function handleTimerTick(event) {
 
 // Event: Answer question
 
-// function handleAnswer(event) {
-//   console.log("Question answered");
-// }
-// answerButtonEl.addEventListener("click", handleAnswer);
-
 // Event: Game ends
 
 function handleGameEnd() {
   console.log("Game ended");
+  clearInterval(timer);
+  timer = null;
+
+  hideElement(gameBoardEl)
+  showElement(gameEndEl);
+  
 }
 
 // Event: Submit name & high score
@@ -177,6 +128,5 @@ function updateHighScores(){
 
 }
 
-// }
 
 init();
